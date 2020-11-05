c1439522714f849bc65459471672565b106ad6b7
Marc Berchtold <me@echolot.io>
2020-11-05 16:40:34 +0100

    Fix wrong sample conversion & average

--- RealStereo/TestTone.cs
+++ RealStereo/TestTone.cs
@@ -4,6 +4,8 @@ using NAudio.Wave.SampleProviders;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Windows;
 
 namespace RealStereo
 {
@@ -13,7 +15,7 @@ namespace RealStereo
         private MMDevice outputAudioDevice, inputAudioDevice;
         private IWavePlayer outWavePlayer;
         private WasapiCapture wasapiCapture;
-        private List<float> captureSamples = new List<float>();
+        private readonly List<float> captureSamples = new List<float>();
 
         public TestTone(MMDevice outputAudioDevice, MMDevice inputAudioDevice)
         {
@@ -33,30 +35,10 @@ namespace RealStereo
             wasapiCapture = new WasapiCapture(inputAudioDevice);
             wasapiCapture.DataAvailable += new EventHandler<WaveInEventArgs>(delegate (object o, WaveInEventArgs e)
             {
-                int increaseBy = (wasapiCapture.WaveFormat.BitsPerSample / 8) * wasapiCapture.WaveFormat.Channels;
-                for (int i = 0; i < e.BytesRecorded; i += increaseBy)
+                for (int i = 0; i < e.BytesRecorded; i += 4)
                 {
-                    float scaledSample = 0;
-                    switch(wasapiCapture.WaveFormat.BitsPerSample)
-                    {
-                        case 32:
-                            uint intSample = (uint)((e.Buffer[i + 3] << 24) | (e.Buffer[i+2] << 16) | (e.Buffer[i+1] << 8) | e.Buffer[i]);
-                            scaledSample = intSample / 4294967295f; /// 2147483648f;
-                            break;
-                        case 24:
-                            uint int24Sample = (uint)((e.Buffer[i + 2] << 16) | (e.Buffer[i + 1] << 8) | e.Buffer[i]);
-                            scaledSample = int24Sample / 16777216f; /// 8388608f;
-                            break;
-                        case 16:
-                            ushort shortSample = (ushort)((e.Buffer[i + 1] << 8) | e.Buffer[i]);
-                            scaledSample = shortSample / 65535f;
-                            break;
-                        case 8:
-                            byte byteSample = (byte)e.Buffer[i];
-                            scaledSample = byteSample / 255f;
-                            break;
-                    }
-                    captureSamples.Add(scaledSample);
+                    float sample = BitConverter.ToSingle(e.Buffer, i);
+                    captureSamples.Add(sample);
                 }
             });
 
@@ -87,17 +69,18 @@ namespace RealStereo
         public float GetAverageCaptureVolume()
         {
             int count = captureSamples.Count;
-            int countZeros = 0;
             float sum = 0;
             for (int i = 0; i < count; i++)
             {
-                if (captureSamples[i] == 0)
+                if (captureSamples[i] < 0)
                 {
-                    countZeros++;
+                    sum -= captureSamples[i];
+                } else
+                {
+                    sum += captureSamples[i];
                 }
-                sum += captureSamples[i];
             }
-            return sum / (count-countZeros);
+            return sum / count;
         }
     }
 }
